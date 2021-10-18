fbf9d739ac3ab67f30d8a195d0ff5139c83fa5b0
Yash Kosambia <90350327+yashkosambia14@users.noreply.github.com>
2021-10-18 18:23:10 -0400

    Add files via upload

---
+++ Sprint_1/us_05_test.py
@@ -0,0 +1,20 @@
+import unittest
+from us_05 import US_05
+
+class TEST_US_05(unittest.TestCase):
+    result = US_05()
+    def test_US_05_1(self):
+        desired_output = []
+        self.assertEqual(self.result, desired_output)
+    def test_US_05_2(self):
+        self.assertTrue(self.result.count('US05: Individial @I16@ has death before marriage') == 0)
+    def test_US_05_3(self):
+        self.assertTrue(self.result.count('US05: Individial @I7@ has death before marriage') == 0)
+    def test_US_05_4(self):
+        self.assertFalse(self.result.count('US05: Individial @I8@ has death before marriage') > 0)
+    def test_US_05_5(self):
+        self.assertFalse(self.result.count('US05: Individial @I24@ has death before marriage') > 0)
+
+
+if __name__ == "__main__":
+    unittest.main(exit=False)
---
+++ Sprint_1/us_06_test.py
@@ -0,0 +1,20 @@
+import unittest
+from us_06 import US_06
+
+class TEST_US_06(unittest.TestCase):
+    result = US_06()
+    def test_US_06_1(self):
+        desired_output = []
+        self.assertEqual(self.result, desired_output)
+    def test_US_06_2(self):
+        self.assertTrue(self.result.count('US06: Individial @I16@ has death before divorce') == 0)
+    def test_US_06_3(self):
+        self.assertTrue(self.result.count('US06: Individial @I7@ has death before divorce') == 0)
+    def test_US_06_4(self):
+        self.assertFalse(self.result.count('US06: Individial @I8@ has death before divorce') > 0)
+    def test_US_06_5(self):
+        self.assertFalse(self.result.count('US06: Individial @I24@ has death before divorce') > 0)
+
+
+if __name__ == "__main__":
+    unittest.main(exit=False)
